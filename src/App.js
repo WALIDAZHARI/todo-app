002b2bc15e90c44a52698a621caff6666ed8eb99
WALID AZHARI <walidazhari519@gmail.com>
2022-02-07 20:41:50 +0100

    Completed the Todo List App !!!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+        <div>
+          <TodoList
+            todos={todos}
+            setTodos={setTodos}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
